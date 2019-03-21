352f497f0c08b5399b2e909687fe9dbc1df115c5
Caiomyra <caiomyrapereira@hotmail.com>
2019-03-21 13:00:34 -0300

    Update exercicio01.rb

--- construtores/exercicio01.rb
+++ construtores/exercicio01.rb
@@ -4,27 +4,14 @@ objeto. O cachorro pode latir “au au!” por padrão ou
 personalizado através de parâmetro.
 Teste ao menos com 2 objetos
 =end
-
 class Cachorro 
-  @nome = nil;
-  @raça = nil;
+ 
+ attr_accessor:nome; # exe: (def nome=(nome); @nome=nome; end) e (def nome; @nome; end)
+ attr_reader:raça; # exe:def raça; @raça; end
  def initialize(nome="fulano",raça="viralata") #metedos construtores
   @nome = nome;
   @raça = raça;        
  end 
-
-   def nome=(nome)
-   	@nome = nome
-   end
-
-   def nome
-    @nome 
-   end
-
-   def raça
-   	@raça
-   end
-
 
    def latir(latir = "AuAu!")
    	 puts latir
@@ -35,12 +22,12 @@ class Cachorro
 cachorro1 = Cachorro.new("mine","pug");
 
 cachorro2 =Cachorro.new
-#cachorro2.raça = "pitbull" #error ,não pod mudar  raça
+#cachorro2.raça = "ddsad" #->error ,EX:não pod altera  raça
 
-puts cachorro1.nome
-puts cachorro1.raça;
-cachorro1.latir("Au Au Au!");
+puts cachorro1.nome #->mine
+puts cachorro1.raça; #->pug
+ cachorro1.latir("Au Au Au!");#->Au Au Au!
 
-puts cachorro2.nome
-puts cachorro2.raça;
-cachorro2.latir();
+puts cachorro2.nome #->fulano
+puts cachorro2.raça; #->viralata
+cachorro2.latir(); #->AuAu!
